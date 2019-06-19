154ffc8d24f9492d701fa179ba7d0cd66b25025f
jmhart <jeffhart91@gmail.com>
2019-06-19 09:33:25 -0500

    Removed print statement

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ model_ids = models.model_ids
 
 model_names = []
 for model_id in model_ids:
-    print(model_id)
     model_names.append((model_id, get_model_name(model_id)))
 
 models.write_model_names(model_names)
